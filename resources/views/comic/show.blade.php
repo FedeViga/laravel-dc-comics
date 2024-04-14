c005409aadaadb3a97db4408b29d9846c7ba4460
federico_viganò <fede_viga@hotmail.it>
2024-04-14 11:09:16 +0200

    aggiunto bonus - fine esercizio

--- resources/views/comic/show.blade.php
+++ resources/views/comic/show.blade.php
@@ -38,11 +38,34 @@
                     </ul>
                     <div class="d-flex justify-content-center gap-2">
                         <a href="{{route('comics.edit', $comic->id)}}" class="btn btn-outline-warning">Edit</a>
-                        <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
-                            @csrf
-                            @method('DELETE')
-                            <button class="btn btn-outline-danger">Delete</button>
-                        </form>
+
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-outline-danger" data-bs-toggle="modal" data-bs-target="#deleteModal">
+                            Delete
+                        </button>
+                        
+                        <!-- Modal -->
+                        <div class="modal" tabindex="-1" id="deleteModal">
+                            <div class="modal-dialog modal-dialog-centered">
+                              <div class="modal-content">
+                                <div class="modal-header">
+                                  <h5 class="modal-title">Delete Comic</h5>
+                                  <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                </div>
+                                <div class="modal-body">
+                                  <p>Are you sure you want to delete the comic {{$comic->title}}?</p>
+                                </div>
+                                <div class="modal-footer">
+                                  <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                  <form action="{{route('comics.destroy', $comic->id)}}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button class="btn btn-danger">Delete</button>
+                                </form>
+                                </div>
+                              </div>
+                            </div>
+                          </div>
                     </div>
                 </div>
             </div>
