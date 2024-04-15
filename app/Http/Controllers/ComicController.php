9f000ece43a0314bccb5adb5cb802ffa45e2f051
federico_viganò <fede_viga@hotmail.it>
2024-04-15 15:51:17 +0200

    completato esercizio senza bonus

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Comic;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ComicController extends Controller
 {
@@ -31,18 +32,7 @@ class ComicController extends Controller
     public function store(Request $request)
     {
 
-        $request->validate([
-            'title' =>'required|max:255',
-            'description' =>'required|max:2000',
-            'thumb' =>'required|max:1000',
-            'price' =>'required|max:10',
-            'series' =>'required|max:50',
-            'sale_date' =>'required',
-            'type' =>'required|max:50',
-            'artists' =>'required|max:1000',
-            'writers' =>'required|max:1000',
-        ]);
-
+        $this->validation($request->all());
 
         $newComic = new Comic();
 
@@ -85,20 +75,8 @@ class ComicController extends Controller
     public function update(Request $request, Comic $comic)
     {
 
+        $this->validation($request->all());
 
-        $request->validate([
-            'title' =>'required|max:255',
-            'description' =>'required|max:2000',
-            'thumb' =>'required|max:1000',
-            'price' =>'required|max:10',
-            'series' =>'required|max:50',
-            'sale_date' =>'required',
-            'type' =>'required|max:50',
-            'artists' =>'required|max:1000',
-            'writers' =>'required|max:1000',
-        ]);
-
-        
         $comic->title = $request->title;
         $comic->description = $request->description;
         $comic->thumb = $request->thumb;
@@ -122,5 +100,21 @@ class ComicController extends Controller
         $comic->delete();
 
         return redirect()->route('comics.index');
+    }
+
+
+    private function validation($data) {
+
+        $validator = Validator::make($data, [
+            'title' =>'required|max:255',
+            'description' =>'required|max:2000',
+            'thumb' =>'required|max:1000',
+            'price' =>'required|max:10',
+            'series' =>'required|max:50',
+            'sale_date' =>'required',
+            'type' =>'required|max:50',
+            'artists' =>'required|max:1000',
+            'writers' =>'required|max:1000'
+        ])->validate();
     }
 }
